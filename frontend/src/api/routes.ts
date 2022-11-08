3a202a38c49bed8f279f42a090c8dca524229b82
Antonio <antoniojosedonishung@gmail.com>
2022-11-07 21:41:32 -0500

    Anti panics

--- frontend/src/api/routes.ts
+++ frontend/src/api/routes.ts
@@ -2,15 +2,19 @@ import { apiBaseURL } from "./config";
 import type { Filter, ArticleResult } from "./types";
 
 export async function articles(page: number, filter: Filter): Promise<ArticleResult> {
-    const response = await fetch(apiBaseURL + `/articles/${page}`, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(filter)
-    });
-    const results = await response.json();
-    return results;
+    try {
+        const response = await fetch(apiBaseURL + `/articles/${page}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(filter)
+        });
+        const results = await response.json();
+        return results;
+    } catch {
+        return {pages: 1, articles: []};
+    }
 }
 
 export async function register(
